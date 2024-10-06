7b739d89724d2d21a40736b872247fb3296c494a
Hurikake0000 <goddongmin@nate.com>
2024-10-06 21:42:25 +0900

    test

---
+++ hello.py
@@ -0,0 +1,14 @@
+def solution(num1, num2):
+    if (-50000 <= num1 <= 50000) and (-50000 <= num2 <= 50000):
+        answer = num1 - num2
+    else:
+        answer = None 
+
+    return answer
+
+print(solution(600, 200)) 
+
+
+
+
+
